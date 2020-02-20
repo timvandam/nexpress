475bc6d0cf4acfcf27e0e09a8b4dce05f4572b1e
Tim van Dam <timvandamcs@gmail.com>
2020-02-20 19:05:56 +0100

    split minecraft packet handler from packet serializer

--- examples/minecraft/index.js
+++ examples/minecraft/index.js
@@ -1,48 +1,12 @@
-const debug = require('debug')('nexpress:examples:minecraft')
 
 const Nexpress = require('../../index')
 const Packet = require('./packet')
 
+const packetHandler = require('./packetHandler')
+
 const app = new Nexpress()
 
-// Track the state of each socket (https://wiki.vg/Protocol)
-const socketState = new Map()
-
-// Server status
-const SERVER_STATUS = {
-  version: {
-    name: '1.15.2!',
-    protocol: 578
-  },
-  players: {
-    max: 321,
-    online: 123,
-    sample: []
-  },
-  description: {
-    text: 'Hello World!',
-    color: 'green',
-    bold: true
-  }
-}
-
-// Disconnect message
-const DISCONNECT_MSG = {
-  text: 'Made with Nexpress!',
-  color: 'yellow',
-  bold: true
-}
-
-// Default socket state to Handshaking
-app.use((data, res, next) => {
-  if (!socketState.has(res.socket)) socketState.set(res.socket, 'Handshaking')
-  next()
-})
-
-// Create a pipeline that will handle individual packets
-const packetHandler = new Nexpress.Pipeline()
-
-// Read incoming data. Put each packet it contains in the packetHandler
+// Serialize incoming data into individual packets and enter them into the packet handler pipeline
 app.use((data, res, next) => {
   for (let i = 0; i < data.buffer.length;) {
     const packet = new Packet(data.buffer.slice(i))
@@ -63,81 +27,6 @@ app.use((data, res, next) => {
 
     i = offset + packet.length
   }
-})
-
-// Handshake packet (0x00, state = handshaking)
-packetHandler.use(0x00, (data, res, next) => {
-  if (socketState.get(res.socket) !== 'Handshaking') return next()
-  debug('received handshake')
-
-  const [,,, nextState] = data.packet.read(Packet.VarInt, Packet.String, Packet.UShort, Packet.VarInt)
-
-  if (nextState.value === 1) socketState.set(res.socket, 'Status')
-  if (nextState.value === 2) socketState.set(res.socket, 'Login')
-})
-
-// Status packet (0x00, state = Status)
-packetHandler.use(0x00, (data, res, next) => {
-  if (socketState.get(res.socket) !== 'Status') return next()
-  debug('received status request')
-
-  const packetId = new Packet.VarInt(0x00)
-  const jsonResponse = new Packet.String(JSON.stringify(SERVER_STATUS))
-  const packetLength = new Packet.VarInt(packetId.length + jsonResponse.length)
-
-  // Write a server status packet
-  const packet = new Packet()
-  packet.write(
-    packetLength,
-    packetId,
-    jsonResponse
-  )
-
-  debug('sending server status')
-  res.send(packet.buffer)
-})
-
-// Ping packet (0x01, state = Status)
-packetHandler.use(0x01, (data, res, next) => {
-  if (socketState.get(res.socket) !== 'Status') return next()
-  debug('received ping')
-
-  const [payload] = data.packet.read(Packet.Long)
-
-  const packetId = new Packet.VarInt(0x01)
-  const packetLength = new Packet.VarInt(packetId.length + payload.length)
-
-  // Write a pong packet
-  const packet = new Packet()
-  packet.write(
-    packetLength,
-    packetId,
-    payload
-  )
-
-  debug('sending pong')
-  res.send(packet.buffer)
-})
-
-// Login packet (0x00, state = Login)
-packetHandler.use(0x00, (data, res, next) => {
-  if (socketState.get(res.socket) !== 'Login') return next()
-  const [username] = data.packet.read(Packet.String)
-  debug('received login request from %o', username.value)
-
-  const packetId = new Packet.VarInt(0x00)
-  const reason = new Packet.String(JSON.stringify(DISCONNECT_MSG))
-  const packetLength = new Packet.VarInt(packetId.length + reason.length)
-
-  const packet = new Packet()
-  packet.write(
-    packetLength,
-    packetId,
-    reason
-  )
-
-  debug('disconected %o', username.value)
-  res.send(packet.buffer)
 })
 
 app.listen(25565)
